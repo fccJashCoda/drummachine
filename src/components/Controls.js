21cb2406842e7628b444befedc4725dc92bb5b6a
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-21 20:32:59 +0100

    layout blocked in

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -3,29 +3,34 @@ import Display from './Display';
 const Controls = ({ display, volume, power, adjustVolume, managePower }) => {
   return (
     <div id="controls">
-      <label htmlFor="volume" id="volumeSlider">
-        Volume
-        <input
-          type="range"
-          min={0}
-          max={1}
-          step={0.01}
-          value={volume}
-          onChange={(e) => adjustVolume(e.target.value)}
-          id="volume"
-        />
-      </label>
-      <label htmlFor="power" id="powerSwitch">
-        On/Off
-        <input
-          type="checkbox"
-          checked={power}
-          onChange={managePower}
-          id="power"
-        />
-      </label>
-      <div id="powerLed" className={power ? 'powerOn' : 'powerOff'}></div>
       <Display display={power ? display : ''} />
+      <div id="control-unit">
+        <label htmlFor="volume" id="volumeSlider">
+          Vol.
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            value={volume}
+            onChange={(e) => adjustVolume(e.target.value)}
+            id="volume"
+          />
+        </label>
+        <label htmlFor="power" id="powerSwitch">
+          Power
+          <div className="switch">
+            <input
+              type="checkbox"
+              checked={power}
+              onChange={managePower}
+              id="power"
+            />
+            <span class="powerSlider"></span>
+          </div>
+        </label>
+        <div id="powerLed" className={power ? 'powerOn' : 'powerOff'}></div>
+      </div>
     </div>
   );
 };
